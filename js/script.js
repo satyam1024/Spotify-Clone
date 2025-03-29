dcd299bdf7fea19be05d5c2f91ac5f0b7f882c16
akashBhardwaj0703 <akashbhardwaj0703@gmail.com>
2025-03-29 16:46:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function secondToMinuteSecond(seconds) {
 async function getSongs(folder) {
 
     currFolder = folder;
-    let a = await fetch(`https://raw.githubusercontent.com/akashBhardwaj0703/Spotify-Clone/main/songs/${folder}/`)
+    let a = await fetch(`https://api.github.com/repos/akashBhardwaj0703/Spotify-Clone/contents/songs/${folder}`);
     let responses = await a.text();
 
 
